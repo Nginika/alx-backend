2c4a8f08aa6e0dc3da61391067fe62522911f79e
Nginika <nginikaugochukwu@gmail.com>
2023-08-01 19:32:02 +0000

    traparameterize

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""basic flask app"""
+""" Basic Flask app """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    """ renders page when / is called """
+    """ Greeting
+
+        Return:
+            Initial template html
+    """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
-"""basic flask babel app"""
-from flask import Flask, render_template, request
+""" Basic Babel setup """
+from flask import Flask, render_template
 from flask_babel import Babel
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
-    """class with configuration for babel"""
-    LANGUAGES = ['en', 'fr']
+    """ Configuration Babel """
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = 'en'
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """ renders page when / is called """
+    """ Greeting
+
+        Return:
+            Initial template html
+    """
     return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
