7789314db0e70ebbd2ef315f62fa3ac877c6260c
Nginika <nginikaugochukwu@gmail.com>
2023-08-30 11:21:28 +0000

    connecting to redis server on local host

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+// Event handler for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event handler for connection errors
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+
